4c80038ae1c72e18950d2201a8f7f7a9d0cfe220
tingdaodi <11264697+tingdaodi@users.noreply.github.com>
2020-07-09 18:05:10 +0800

    Init

--- tellus-permission-cloud-starter/src/main/java/com/tellus/permission/oauth2/authentication/IAuthenticationSuccessHandler.java
+++ tellus-permission-cloud-starter/src/main/java/com/tellus/permission/oauth2/authentication/IAuthenticationSuccessHandler.java
@@ -6,10 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 认证成功后处理器
@@ -21,7 +19,7 @@ import java.io.IOException;
 public class IAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) {
         ResponseStatusUtils.writer(response, Result.success("Login successful"));
     }
 }
--- tellus-permission-cloud-starter/src/main/java/com/tellus/permission/oauth2/authentication/ILogoutSuccessHandler.java
+++ tellus-permission-cloud-starter/src/main/java/com/tellus/permission/oauth2/authentication/ILogoutSuccessHandler.java
@@ -6,15 +6,12 @@ import com.tellus.support.Result;
 import com.tellus.toolkit.util.ResponseStatusUtils;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 注销成功处理器
@@ -37,7 +34,7 @@ public class ILogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
-                                Authentication authentication) throws IOException, ServletException {
+                                Authentication authentication) {
         String authHeader = request.getHeader(tellusSecurityProperties.getTokenHeader());
         if (!Strings.isNullOrEmpty(authHeader) && authHeader.startsWith(tellusSecurityProperties.getTokenPrefix())) {
             final String xToken = authHeader.substring(tellusSecurityProperties.getTokenPrefix().length() + 1);
